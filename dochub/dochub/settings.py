28651f2264afafa4aac87659a874f2bb2eb7b6c7
ddreset <zhangyd96@gmail.com>
2019-11-16 11:43:05 +0200

    remove staticapp setting

--- dochub/dochub/settings.py
+++ dochub/dochub/settings.py
@@ -36,7 +36,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
     'dhdata.apps.DhdataConfig',
     'rest_framework'
 ]
@@ -114,9 +113,3 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.2/howto/static-files/
-
-STATIC_URL = '/static/'
