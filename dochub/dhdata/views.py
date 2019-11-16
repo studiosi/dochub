eb0e38e2d33fcc21b53545cf75870910e83c386b
ddreset <zhangyd96@gmail.com>
2019-11-16 09:09:30 +0200

    ADD rest_framework and /doctors /reviews

--- dochub/dhdata/views.py
+++ dochub/dhdata/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Doctor, Review
+from .serializers import DoctorSerializer, ReviewSerializer
 
 # Create your views here.
+class DoctorViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = Doctor.objects.all()
+    serializer_class = DoctorSerializer
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
--- dochub/dochub/urls.py
+++ dochub/dochub/urls.py
@@ -14,16 +14,24 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 import dhdata.doctorUtils as doctorUtils
+
+from rest_framework import routers
+from dhdata import views
+
+router = routers.DefaultRouter()
+router.register(r'doctors', views.DoctorViewSet)
+router.register(r'reviews', views.ReviewViewSet)
 
 urlpatterns = [
     # Admin
     path('admin/', admin.site.urls),
 
     # Doctors
-    path('doctors/', doctorUtils.getDoctorList),
-    path('doctors/<int:id>', doctorUtils.manageDoctor),
+    # path('doctors/', doctorUtils.getDoctorList),
+    # path('doctors/<int:id>', doctorUtils.manageDoctor),
+    path('', include(router.urls)),
 ]
 
